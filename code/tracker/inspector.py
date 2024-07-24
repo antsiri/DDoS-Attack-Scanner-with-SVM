09d0ccf26ba64189db6b7d29674e82184388165f
AntonioSirignano <57763470+antsiri@users.noreply.github.com>
2024-07-24 11:39:06 +0200

    Update inspector.py

--- code/tracker/inspector.py
+++ code/tracker/inspector.py
@@ -7,11 +7,11 @@ from sklearn.metrics import confusion_matrix, classification_report
 import joblib
 import warning
 
-filename = '/home/antonio/Desktop/NCI/code/ml/model.sav'
+filename = './ml/model.sav'
 classifier = joblib.load(filename)
-dt_realtime = pd.read_csv('/home/antonio/Desktop/NCI/code/tracker/rt_data.csv')
+dt_realtime = pd.read_csv('rt_data.csv')
 dt_realtime.fillna(0, inplace=True)
 result = classifier.predict(dt_realtime)
 
-with open('/home/antonio/Desktop/NCI/code/tracker/.result', 'w') as f:
+with open('.result', 'w') as f:
     f.write(str(result[0]))
